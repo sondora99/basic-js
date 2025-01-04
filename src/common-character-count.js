f2864ad880088b143daa3f38553f3fd02e68bca0
sondora99 <marymikser@gmail.com>
2025-01-04 22:08:51 +0300

    feat: solve 3 tasks

--- src/common-character-count.js
+++ src/common-character-count.js
@@ -11,9 +11,17 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For s1 = "aabcc" and s2 = "adcaa", the output should be 3
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
-function getCommonCharacterCount(/* s1, s2 */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getCommonCharacterCount(s1, s2) {
+  let count = 0;
+  const s2Arr = s2.split('');
+  for (let i of s1) {
+    const temp = s2Arr.indexOf(i);
+    if (temp != -1) {
+      count += 1;
+      s2Arr.splice(temp, 1)
+    }
+  }
+  return count;
 }
 
 module.exports = {
--- src/delete-digit.js
+++ src/delete-digit.js
@@ -11,9 +11,20 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For n = 152, the output should be 52
  *
  */
-function deleteDigit(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function deleteDigit(n) {
+  const str = n.toString();
+  const strArr = str.split('');
+  let numArr = strArr.map(element => Number(element));
+  let maxNum = 0;
+  for (let i = 0; i < numArr.length; i++) {
+    let tempArr = numArr.slice();
+    delete tempArr[i];
+    let res = Number(tempArr.join(''))
+    if (res > maxNum) {
+      maxNum = res;
+    }
+  }
+  return maxNum;
 }
 
 module.exports = {
