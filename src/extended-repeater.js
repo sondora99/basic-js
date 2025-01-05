7cd0363451189eca6abbfc1ab845b77f7a08bb8b
sondora99 <marymikser@gmail.com>
2025-01-05 22:42:29 +0300

    feat: solve 11 tasks

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,9 +15,18 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+  const repeatTimes = options.repeatTimes;
+  const separator = options.separator || '+';
+  const addition = 'addition' in options ? String(options.addition) : '';
+  const additionRepeatTimes = options.additionRepeatTimes || 1;
+  const additionSeparator = options.additionSeparator || '|';
+
+  let addPart = Array(additionRepeatTimes).fill(addition).join(additionSeparator);
+  let mainPart = Array(repeatTimes).fill(str + addPart).join(separator);
+
+  return mainPart
+
 }
 
 module.exports = {
--- src/sum-digits.js
+++ src/sum-digits.js
@@ -12,9 +12,20 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For 91, the result should be 1 (9 + 1 = 10, 1 + 0 = 1)
  *
  */
-function getSumOfDigits(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getSumOfDigits(n) {
+  if (n < 10) {
+    return n
+  };
+  
+  while (n >= 10){
+    let temp = 0;
+    while (n > 0) {
+      temp += n % 10;
+      n = Math.floor(n / 10)
+    }
+  n = temp;
+  }
+  return n
 }
 
 module.exports = {
