ff22a8efae44c30610fbbef256a4f5aa298bdda8
sondora99 <marymikser@gmail.com>
2025-01-05 17:04:33 +0300

    feat: solve 6 tasks

--- src/dns-stats.js
+++ src/dns-stats.js
@@ -22,9 +22,20 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getDNSStats(domains) {
+  let result = {};
+
+  domains.forEach(domain => {
+    const temp = domain.split('.').reverse();
+    let acc = '';
+
+    temp.forEach(element => {
+      acc += `.${element}`;
+      result[acc] = (result[acc] || 0) + 1;
+    })
+  })
+  return result;
+
 }
 
 module.exports = {
--- src/encode-line.js
+++ src/encode-line.js
@@ -10,9 +10,23 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function encodeLine(str) {
+  let result = '';
+  let count = 1;
+  for (let i = 0; i < str.length; i++){
+    let temp = str[i];
+    if (str[i + 1] === temp){
+      count += 1
+    } else {
+      if (count === 1) {
+        result += temp
+      } else {
+      result += count + temp
+      count = 1; }
+    }
+  }
+  return result;
+
 }
 
 module.exports = {
