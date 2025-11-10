31579e89c7d75cb2c7b6f4e471aaca5889aaf889
sondora99 <marymikser@gmail.com>
2025-11-10 20:08:44 +0300

    feat: solve what-season task

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,12 +13,7 @@ const { NotImplementedError } = require('../lib');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  *
  */
-<<<<<<< HEAD
 function transform(arr) {
-  /*--discard-next
-  --discard-prev
-  --double-next
-  --double-prev*/
   if (!Array.isArray(arr)) {
      throw new Error("'arr' parameter must be an instance of the Array!"); }
   
@@ -56,12 +51,6 @@ function transform(arr) {
       return result;
 
   }
-=======
-function transform(/* arr */) {
-  // Remove line below and write your code here
-  throw new NotImplementedError('Not implemented');
-}
->>>>>>> upstream/master
 
 module.exports = {
   transform
--- src/what-season.js
+++ src/what-season.js
@@ -11,9 +11,30 @@ const { NotImplementedError } = require('../lib');
  * getSeason(new Date(2020, 02, 31)) => 'spring'
  *
  */
-function getSeason(/* date */) {
+function getSeason(date) {
   // Remove line below and write your code here
-  throw new NotImplementedError('Not implemented');
+  if (!date) {
+    return 'Unable to determine the time of year!';
+  }
+
+  const dat = new Date(date);
+
+  if (isNaN(dat.getTime())) {
+    return 'Unable to determine the time of year!';
+  }
+
+  const month = dat.getMonth();
+  let result;
+  if (month >= 0 && month < 2 || month === 11){
+    result = 'winter'
+  } else if (month >= 2 && month < 5) {
+    result = 'spring'
+  } else if (month >= 5 && month < 8) {
+    result = 'summer'
+  } else if (month >= 8 && month <= 10) {
+    result = 'autumn'
+  } else (result = 'Invalid date!')
+  return result;
 }
 
 module.exports = {
